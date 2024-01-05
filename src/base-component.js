f99fd7a8f079596fe8d829ea30f2688ce4759a4c
Yauheni Tsurankou <ikk.pott@gmail.com>
2024-01-05 05:30:57 +0300

    feat: add component and function impl

---
+++ src/base-component.js
@@ -0,0 +1,143 @@
+/**
+ * Represents a component for creating and managing HTML elements with additional functionalities.
+ * @class
+ */
+class Component {
+	/**
+	 * @private
+	 * @type {Array<Component>} - An array to store child components.
+	 */
+	#children = []
+
+	/**
+	 * @private
+	 * @type {HTMLElement} - The HTML node associated with the component.
+	 */
+	#node = null
+	/**
+	 * Creates a new Component.
+	 * @constructor
+	 * @param {Object} options - The options for creating the component.
+	 * @param {string} options.tag - HTML element tag (default is 'div').
+	 * @param {string} options.className - CSS class name for the element.
+	 * @param {string} options.content - Text content of the element.
+	 * @param {...Component} children - Child components to be appended.
+	 */
+
+	constructor({ tag, className, content }, ...children) {
+		const node = document.createElement(tag ?? 'div')
+		node.className = className ?? ''
+		node.textContent = content ?? ''
+		this.#node = node
+
+		if (children) {
+			this.appendChildren(children)
+		}
+	}
+	/**
+	 * Appends a child component to the current component.
+	 * @param {Component} child - The child component to be appended.
+	 */
+
+	append(child) {
+		this.#children.push(child)
+		this.#node.append(child.getNode())
+	}
+	/**
+	 * Appends an array of child components to the current component.
+	 * @param {Array<Component>} children - Array of child components to be appended.
+	 */
+
+	appendChildren(children) {
+		children.forEach(el => {
+			this.append(el)
+		})
+	}
+	/**
+	 * Returns the HTML node associated with the component.
+	 * @returns {HTMLElement} - The HTML node.
+	 */
+
+	getNode() {
+		return this.#node
+	}
+	/**
+	 * Returns an array of child components.
+	 * @returns {Array<Component>} - Array of child components.
+	 */
+
+	getChildren() {
+		return this.#children
+	}
+	/**
+	 * Sets the text content of the component.
+	 * @param {string} content - The text content to be set.
+	 */
+
+	setTextContent(content) {
+		this.#node.textContent = content
+	}
+	/**
+	 * Sets an attribute on the component's HTML node.
+	 * @param {string} attribute - The attribute to set.
+	 * @param {string} value - The value to set for the attribute.
+	 */
+
+	setAttribute(attribute, value) {
+		this.#node.setAttribute(attribute, value)
+	}
+	/**
+	 * Removes an attribute from the component's HTML node.
+	 * @param {string} attribute - The attribute to remove.
+	 */
+
+	removeAttribute(attribute) {
+		this.#node.removeAttribute(attribute)
+	}
+	/**
+	 * Toggles the presence of a CSS class on the component's HTML node.
+	 * @param {string} className - The class name to toggle.
+	 */
+
+	toggleClass(className) {
+		this.#node.classList.toggle(className)
+	}
+	/**
+	 * Adds an event listener to the component's HTML node.
+	 * @param {string} event - The event type to listen for.
+	 * @param {EventListener} listener - The callback function to be executed when the event occurs.
+	 * @param {boolean|AddEventListenerOptions} [options=false] - An options object specifying characteristics of the event listener.
+	 */
+
+	addListener(event, listener, options = false) {
+		this.#node.addEventListener(event, listener, options)
+	}
+	/**
+	 * Removes an event listener from the component's HTML node.
+	 * @param {string} event - The event type for which to remove the listener.
+	 * @param {EventListener} listener - The listener function to be removed.
+	 * @param {boolean|EventListenerOptions} [options=false] - Options that were used when adding the listener.
+	 */
+
+	removeListener(event, listener, options = false) {
+		this.#node.removeEventListener(event, listener, options)
+	}
+	/**
+	 * Destroys all child components associated with the current component.
+	 */
+
+	destroyChildren() {
+		this.#children.forEach(child => {
+			child.destroy()
+		})
+		this.#children.length = 0
+	}
+	/**
+	 * Destroys the current component and removes its HTML node from the DOM.
+	 */
+
+	destroy() {
+		this.destroyChildren()
+		this.#node.remove()
+	}
+}
---
+++ src/create-element.js
@@ -0,0 +1,26 @@
+/**
+ * Function to create an HTML element with specified parameters.
+ * @param {Object} options - Object containing parameters for creating the element.
+ * @param {string} options.tag - HTML element tag (default is 'div').
+ * @param {string} options.text - Text content of the element (default is an empty string).
+ * @param {HTMLElement} options.parent - Parent HTML element to which the created element is appended (default is null).
+ * @param {Array} options.classes - Array of classes to be added to the created element (default is an empty array).
+ * @returns {HTMLElement} - Created HTML element.
+ */
+function createElement(options) {
+	// Default values
+	const { tag = 'div', text = '', parent, classes = [] } = options
+
+	const element = document.createElement(tag)
+	element.textContent = text // Adding classes if provided
+
+	if (classes.length > 0) {
+		element.classList.add(...classes)
+	} // Adding the element to the parent element if necessary
+
+	if (parent != null) {
+		parent.appendChild(element)
+	} // Returning the created element for further manipulations
+
+	return element
+}
